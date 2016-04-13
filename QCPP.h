376a09d53e7e735d44f0116a58ad310c7f32f744
Phirasit <phirasitsharp@gmail.com>
2016-04-13 22:18:16 +0700

    add more functions (Hadamard Gate)

--- QCPP.h
+++ QCPP.h
@@ -3,16 +3,19 @@
 
 #include <algorithm>
 #include <complex>
-#include <cstdarg>
+#include <cmath>
 #include <functional>
 #include <vector>
+
+const std::complex<double> __quantum_sqrt_half = 1 / sqrt(2.0);
 
 class Quantum {
     
     private:
 
+
     	size_t size;
-    	std::vector< std::complex<double> > data;
+    	std::vector< std::complex<double> > data, buffer;
 
     	void checkStatus(void);
 
@@ -22,16 +25,28 @@ class Quantum {
     	void addQubits(Quantum);
 
     	void Hadamard(size_t);
-    	void Hadamard(size_t, size_t...);
+    	template<typename... Args> void Hadamard(size_t, Args...);
     	void Hadamard(std::vector<size_t>);
-    	
+
     	double get_probability(size_t);
     	size_t get_state(void);
+    	std::complex<double> get_phase(size_t);
 
-    	Quantum(size_t);
+    	Quantum(size_t = 1u);
     	Quantum(std::vector< std::complex<double> >);
     	Quantum(size_t, std::vector< std::pair< size_t, std::complex<double> > >);
 
+    	~Quantum(void);
 };
+
+// some functions that i can't put in other files
+// it's because 'template' that makes this code less beautiful
+
+// apply some Hadamard gates to some data
+template<typename... Args> 
+void Quantum::Hadamard(size_t idx, Args... args) {
+	Hadamard(idx);
+	Hadamard(args...);
+}
 
 #endif
