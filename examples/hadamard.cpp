f268f766223f20f1c34961c90a4bf47cb68f8a97
Phirasit <phirasitsharp@gmail.com>
2016-04-14 15:37:26 +0700

    add example of using hadamard gates

---
+++ examples/hadamard.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <ctime>
+#include <QCPP.h>
+
+int main() {
+
+	// make it more non-deterministic
+	srand(time(NULL));
+
+	// initialize Quantum state with 5 qubits
+	Quantum qubits(5);
+
+	// turns all qubits into every state with same probabilities
+	qubits.Hadamard(0, 1, 2, 3, 4);
+	// or
+	// for(int i = 0;i < qubits.size();i++) qubits.Hadamard(i);
+	// or
+	// qubits.Hadamard({0, 1, 2, 3, 4});
+	// would result the same
+
+	// the possibility should be the same
+	for(int i = 0;i < (1 << qubits.size());i++) {
+		std::cout << "Possibility of being " << i << " : " << qubits.getProbability(i) << std::endl;
+	}
+
+	// try to observe
+	for(int i = 0;i < 10;i++) {
+		std::cout << "State of qubits : " << qubits.getState() << std::endl;
+	}
+
+	return 0;
+}
