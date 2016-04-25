0172f0ba8120878526eeffd89858fc178de251e7
Phirasit <phirasitsharp@gmail.com>
2016-04-25 13:20:57 +0700

    add new example QFT(Quantum Fourier Transform)

---
+++ examples/QFT.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+#include <QCPP.h>
+
+using namespace std;
+
+int main() {
+
+	Quantum qubits(5);
+
+	for(int i = 0;i < qubits.size();i++) {
+		qubits.hadamard(i);
+		for(int j = i+1;j < qubits.size();j++) {
+			qubits.controlled(qubits.phaseShiftFunc(i, 2 * M_PI / pow(2, j-i+1)), j);
+		}
+	}
+	
+	for(int i = 0, j = qubits.size()-1;i < j;i++, j--) {
+		qubits.swap(i, j);
+	}
+
+	for(int i = 0;i < (1 << qubits.size());i++) {
+		cout << "Possibility of being " << i << " = " << qubits.getPhase(i) << std::endl;
+	}
+	return 0;
+}
